e409ae96f6406c07ab1a78adfcee788c6327e7ac
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-02-17 02:55:58 +0600

    delete mutation added

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -64,6 +64,26 @@ class UpdateTrack(graphene.Mutation):
         return UpdateTrack(track=track)
 
 
+class DeleteTrack(graphene.Mutation):
+    track_id = graphene.Int()
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        # Getting authenticated user
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+
+        if track.created_by != user:
+            raise Exception('You are not permitted to delete this track')
+
+        track.delete()
+
+        return DeleteTrack(track_id=track_id)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
     update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
