851c4cb66573c1e71a06ce1b5b9b5c9ffdfd38ea
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-02-17 20:41:30 +0600

    configured apollo client to the frontend for query and mutation

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,6 +1,29 @@
-import React from "react";
-import withRoot from "./withRoot";
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from '@apollo/react-components';
 
-const Root = () => <div>Root</div>;
+import withRoot from './withRoot';
+
+const GET_TRACKS_QUERY = gql`
+  {
+    tracks {
+      id
+      title
+      description
+      url
+    }
+  }
+`;
+
+const Root = () => (
+  <Query query={GET_TRACKS_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <div>{JSON.stringify(data)}</div>;
+    }}
+  </Query>
+);
 
 export default withRoot(Root);
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/react-components';
+import ApolloClient from 'apollo-boost';
+
 import Root from './Root';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/'
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
